6eb2bd69083df1380e8c98332077222a96af0c03
dcrosby42 <dcrosby42@48d1a0da-1ed6-4f58-8415-457aad5d95da>
2007-11-13 04:18:27 +0000

    Updated sample code to include concrete stubbing and mocking
    git-svn-id: http://hardmock.rubyforge.org/svn/trunk@42 48d1a0da-1ed6-4f58-8415-457aad5d95da

--- homepage/demo.rb
+++ homepage/demo.rb
@@ -1,21 +1,27 @@
-require 'hardmock'
+  require 'hardmock'
 
-class MyTest < Test::Unit::TestCase
+  class MyTest < Test::Unit::TestCase
 
-  def setup
-    create_mocks :garage, :car
+    def setup
+      create_mocks :garage, :car
+    end
+
+    def test_the_mocks
+      # Set some expectations
+      @garage.expects.open_door
+      @car.expects.start(:choke)
+      @car.expects.drive(:reverse, 5.mph)
+
+      # Execute the code (normally your own classes do this)
+      @garage.open_door  
+      @car.start :choke
+      @car.drive :reverse, 5.mph
+    end
+
+    def test_the_concrete_stubbing_and_mocking
+      SchoolBus.stubs!(:color, "yellow")
+      Entanglement.expects!(:new, "my false entanglement")
+      cat.expects!(:hungry).returns("meow")
+    end
+
   end
-
-  def test_the_mocks
-    # Set some expectations
-    @garage.expects.open_door
-    @car.expects.start(:choke)
-    @car.expects.drive(:reverse, 5.mph)
-
-    # Execute the code (normally your own classes do this)
-    @garage.open_door  
-    @car.start :choke
-    @car.drive :reverse, 5.mph
-  end
-
-end
